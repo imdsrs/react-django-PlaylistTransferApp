94f9b057b2997c581b11e2d596b7aaab814a69c7
Shashank Singh <ec22267@qmul.ac.uk>
2023-08-23 04:25:54 +0100

    added new images and modified animation

--- react_app/src/components/TransferComplete.js
+++ react_app/src/components/TransferComplete.js
@@ -4,8 +4,8 @@ import CompleteGif from "../assets/completeDark.gif";
 const TransferComplete = () => {
     return (
         <div class="text-gray-400 bg-gray-900 body-font h-[52vh]">
-            <span class="flex flex-wrap justify-center items-center">
-                <img src={CompleteGif} alt="Transfer Complete" />
+            <span class="flex flex-wrap justify-center items-center scale-[150%]">
+                <img className="" src={CompleteGif} alt="Transfer Complete" />
             </span>
         </div>
     );
